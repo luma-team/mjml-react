5942cb8cd868e55bb2860c6d2850150d9610ae05
Mohammad Bagher Abiat <zorofight94@gmail.com>
2023-10-28 00:46:21 +0330

    format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ['src/index.tsx'],
+  entry: ["src/index.tsx"],
   minify: true,
   format: ["esm", "cjs"],
   dts: true,
